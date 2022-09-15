f8e72cd51991fe6c4e1f7a96c84b3d9db3b9bcca
Abdul Awal <abdulawalarif0011@gmail.com>
2022-09-15 12:19:18 +0600

    added pagination search UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,9 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import '../consts/global_colors.dart';
+import '../widgets/appbar_icons.dart';
+import '../widgets/sale_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,15 +13,95 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late TextEditingController _textEditingController;
+
+  @override
+  void initState() {
+    _textEditingController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
       },
-      child: const Scaffold(
-        body: Center(child: Text("Welcome")),
-      ),
+      child: Scaffold(
+          appBar: AppBar(
+            //elevation: 4,
+            title: const Text('Home'),
+            leading: AppBarIcons(
+              function: () {},
+              icon: IconlyBold.category,
+            ),
+            actions: [
+              AppBarIcons(
+                function: () {},
+                icon: IconlyBold.user3,
+              ),
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 18,
+                ),
+                TextField(
+                  controller: _textEditingController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    hintText: "Search",
+                    filled: true,
+                    fillColor: Theme.of(context).cardColor,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide(
+                        color: Theme.of(context).cardColor,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide(
+                        width: 1,
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                    suffixIcon: Icon(
+                      IconlyLight.search,
+                      color: lightIconsColor,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: size.height * 0.25,
+                  child: Swiper(
+                    itemCount: 4,
+                    itemBuilder: (ctx, index) {
+                      return const SaleWidget();
+                    },
+                    pagination: SwiperPagination(
+                      alignment: Alignment.bottomCenter,
+                      builder: DotSwiperPaginationBuilder(
+                        color: Colors.white,
+                        activeColor: Colors.red,
+                      ),
+                    ),
+                    //control: const SwiperControl(),
+                  ),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
